1dc3f7243a1cc1b9231ae27c92eeb653619b8049
giraffe0813 <yemengying1993@foxmail.com>
2016-04-07 18:59:32 +0800

    update

--- src/main/java/com/ymy/algorithms/statistics/MaxSubarraySum.java
+++ src/main/java/com/ymy/algorithms/statistics/MaxSubarraySum.java
@@ -8,8 +8,25 @@ package com.ymy.algorithms.statistics;
  */
 public class MaxSubarraySum {
 
+    /**
+     * ms[i]为以nums[i]结尾的子数组的和的最大值
+     * ms的递归表达式如下:
+     * ms[i] = nums[i] if i = 0
+     * ms[i] = Max{nums[i]+ms[i-1],nums[i]} otherwise
+     * eg:[1,-2,3,4]
+     * ms[0] = nums[0] = 1
+     * ms[1] = ms[0] + nums[1] = -1
+     * ms[2] = nums[2] = 3
+     * ms[3] = ms[2] + nums[3] = 7
+     * ms = [1,-1,3,4]
+     */
     private static int[] ms;
 
+    /**
+     * 利用动态规划
+     * @param nums
+     * @return
+     */
     public static int getMaxSubArraySum(int[] nums){
         if(nums == null || nums.length == 0) return 0;
         if(nums.length == 1) return nums[0];
