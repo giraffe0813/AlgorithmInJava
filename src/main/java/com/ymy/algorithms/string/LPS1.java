9412b33bc6cab6c051ef0c5e168a12f00679c8de
yemengying <yemengying1993@foxmail.com>
2016-04-08 08:58:16 +0800

    add longest palindromicsubstring

--- src/main/java/com/ymy/algorithms/string/LPS1.java
+++ src/main/java/com/ymy/algorithms/string/LPS1.java
@@ -4,5 +4,5 @@ package com.ymy.algorithms.string;
  * 最长回文子序列
  * Created by yemengying on 16/4/7.
  */
-public class LPS {
+public class LPS1 {
 }
---
+++ src/main/java/com/ymy/algorithms/string/LPS2.java
@@ -0,0 +1,9 @@
+package com.ymy.algorithms.string;
+
+/**
+ * 最长回文子字符串
+ * leetcode第5题
+ * Created by yemengying on 16/4/8.
+ */
+public class LPS2 {
+}
