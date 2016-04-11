633308ffd9bdd137fffc3e65061f1601d177652c
giraffe0813 <yemengying1993@foxmail.com>
2016-04-11 14:00:36 +0800

    upate

--- src/main/java/com/ymy/algorithms/string/LPS1.java
+++ src/main/java/com/ymy/algorithms/string/LPS1.java
@@ -2,6 +2,10 @@ package com.ymy.algorithms.string;
 
 /**
  * 最长回文子序列
+ * Example:
+ * String A = " AABCDEBAZ";
+ * 那么最长回文子序列为: ABCBA or ABDBA or ABEBA
+ * 找到一个就可以
  * Created by yemengying on 16/4/7.
  */
 public class LPS1 {
